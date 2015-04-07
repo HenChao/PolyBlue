0c02181513421f12d99de92442d271a32491c329
HenChao <henchao@gmail.com>
2015-04-06 22:56:41 -0400

    Reads port in javascript automatically based on tornado template

--- server.py
+++ server.py
@@ -4,35 +4,30 @@ import tornado.web
 import tornado.template
 import tornado.websocket
 
-clients = dict()
+port = os.getenv('VCAP_APP_PORT', '5000')
 
 class IndexHandler(tornado.web.RequestHandler):
 	@tornado.web.asynchronous
 	def get(self):
 		loader = tornado.template.Loader("templates/")
-		self.write(loader.load("index.html").generate())
+		self.write(loader.load("index.html").generate(app_port=port))
 		self.finish()
 
 class WebSocketHandler(tornado.websocket.WebSocketHandler):
 	def open(self, *args):
-		self.id = self.get_argument("Id")
-		self.stream.set_nodelay(True)
-		clients[self.id] = {"id":self.id, "object": self}
+		print ("Socket connection open")
 
 	def on_message(self, message):
-		print "Client %s received a message: %s" % (self.id, message)
+		print ("Received message: %s" % message)
 
 	def on_close(self):
-		if self.id in clients:
-			del clients[self.id]
+		pass
 
 app = tornado.web.Application([
 	(r'/static/(.*)', tornado.web.StaticFileHandler, {'path': 'static/'}),
 	(r'/', IndexHandler),
 	(r'/socket', WebSocketHandler),
 ])
-
-port = os.getenv('VCAP_APP_PORT', '5000')
 
 if __name__ == "__main__":
 	app.listen(int(port))
