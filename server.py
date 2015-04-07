48288d9c2d6a93bed33b57d3976e34fd757cfb6d
HenChao <henchao@gmail.com>
2015-04-07 00:08:36 -0400

    Call external python script and outputs to web

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 import tornado.ioloop
 import tornado.web
 import tornado.template
@@ -14,12 +15,21 @@ class IndexHandler(tornado.web.RequestHandler):
 		self.finish()
 
 class WebSocketHandler(tornado.websocket.WebSocketHandler):
+	proc = None
+	stdoutput = None
+
 	def open(self, *args):
+		proc = subprocess.Popen('scripts/test.py',
+					stdin=subprocess.PIPE,
+					stdout=subprocess.PIPE,
+					)
+		stdoutput = proc.communicate()
+		self.write_message(stdoutput[0])
 		print ("Socket connection open")
 
 	def on_message(self, message):
 		print ("Received message: %s" % message)
-		self.write_message("The message received was - " + message)
+		#self.write_message("The message received was - " + message)
 
 	def on_close(self):
 		pass
@@ -33,6 +43,7 @@ app = tornado.web.Application([
 if __name__ == "__main__":
 	app.listen(int(port))
 	print("Server running on port: %s" % port)
+
 	try:
 		tornado.ioloop.IOLoop.instance().start()
 	except KeyboardInterrupt:
